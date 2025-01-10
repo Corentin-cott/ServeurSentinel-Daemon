e7aac8b1390b9b4f14f10539f7f27a8be5b47114
Corentin-cott <corentin.cott@gmail.com>
2025-01-10 12:21:16 +0100

    Création du projet et des packages

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
- 
+module github.com/Corentin-cott/ServeurSentinel
+
+go 1.22.2
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
+package main
+
+import (
+    "fmt"
+    // "github.com/Corentin-cott/ServeurSentinel/config"
+    // "github.com/Corentin-cott/ServeurSentinel/internal/console"
+    // "github.com/Corentin-cott/ServeurSentinel/internal/db"
+    // "github.com/Corentin-cott/ServeurSentinel/internal/events"
+)
+
+func main() {
+	fmt.Println("Hello World !")
+}
  
