42eaf73e2286da8194e4b3bcfbeb3644109f150b
Corentin-cott <corentin.cott@gmail.com>
2025-01-16 11:37:08 +0100

    Ajout de commentaire de log pour une meilleure lisibilité

--- internal/triggers/actions.go
+++ internal/triggers/actions.go
@@ -14,14 +14,16 @@ import (
 	"github.com/Corentin-cott/ServeurSentinel/internal/db"
 )
 
-// Écrit une ligne dans un fichier log
+// WriteToLogFile writes a line to a log file
 func WriteToLogFile(logPath, line string) error {
+	// Open the log file
 	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("erreur d'ouverture du fichier log : %v", err)
 	}
 	defer file.Close()
 
+	// Write the line to the log file
 	_, err = file.WriteString(line + "\n")
 	if err != nil {
 		return fmt.Errorf("erreur d'écriture dans le fichier log : %v", err)
@@ -29,77 +31,81 @@ func WriteToLogFile(logPath, line string) error {
 	return nil
 }
 
-// Envoi un message à un serveur Discord
-func SendToDiscord(message string) {
+// SendToDiscord sends a message to a Discord channel
+func SendToDiscord(message string) error {
+	// Get parameters from the configuration
 	botToken := config.AppConfig.Bot.BotToken
 	channelID := config.AppConfig.Bot.DiscordChannelID
 
+	// Checks if one of the parameters is missing
 	switch {
 	case botToken == "" && channelID == "":
 		fmt.Println("Bot token and channel ID not set. Skipping Discord message.")
-		return
+		return fmt.Errorf("ERROR: BOT TOKEN AND CHANNEL ID NOT SET")
 	case botToken == "":
 		fmt.Println("Bot token not set. Skipping Discord message.")
-		return
+		return fmt.Errorf("ERROR: BOT TOKEN NOT SET")
 	case channelID == "":
 		fmt.Println("Channel ID not set. Skipping Discord message.")
-		return
+		return fmt.Errorf("ERROR: CHANNEL ID NOT SET")
 	}
 
+	// Prepare the request to the Discord API
 	apiURL := fmt.Sprintf("https://discord.com/api/v10/channels/%s/messages", channelID)
-
 	type DiscordBotMessage struct {
 		Content string `json:"content"`
 	}
 
+	// Serialize the message to JSON
 	payload := DiscordBotMessage{Content: message}
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Printf("Erreur lors de la sérialisation du message Discord : %v\n", err)
-		return
+		return fmt.Errorf("ERROR WHILE SERIALISING DISCORD MESSAGE: %v", err)
 	}
 
+	// Create the HTTP request to send the message
 	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payloadBytes))
 	if err != nil {
-		fmt.Printf("Erreur lors de la création de la requête : %v\n", err)
-		return
+		return fmt.Errorf("ERROR WHILE CREATING REQUEST TO DISCORD: %v", err)
 	}
 
+	// Set the headers for the request
 	req.Header.Set("Authorization", "Bot "+botToken)
 	req.Header.Set("Content-Type", "application/json")
 
+	// Finally, send the request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Erreur lors de l'envoi à Discord : %v\n", err)
-		return
+		return fmt.Errorf("ERROR WHILE SENDING MESSAGE TO DISCORD: %v", err)
 	}
 	defer resp.Body.Close()
 
+	// Check the response status
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		fmt.Printf("Erreur lors de l'envoi à Discord : Status %d\n", resp.StatusCode)
+		return fmt.Errorf("ERROR WHILE SENDING MESSAGE TO DISCORD, RESPONSE STATUS: %v", resp.Status)
 	} else {
-		fmt.Println("Message envoyé à Discord avec succès.")
+		return nil
 	}
 }
 
-func PlayerJoinAction(line string) {
-	// Extraire les informations du joueur
+// PlayerJoinAction logs the player's connection
+func PlayerJoinAction(line string) error {
+	// Extract the player's name from the line
 	re := regexp.MustCompile(`\[(\d{2}:\d{2}:\d{2})\] \[Server thread/INFO\]: (\w+) joined the game`)
 	matches := re.FindStringSubmatch(line)
 	if len(matches) < 3 {
-		fmt.Println("Erreur lors de l'extraction des informations de connexion")
-		return
+		return fmt.Errorf("ERRROR WHILE EXTRACTING CONNECTION INFO")
 	}
 
-	playerName := matches[2] // Le UUID du joueur est le deuxième groupe
+	playerName := matches[2] // Get the player's name from the regex match
 
-	// Utilisez la nouvelle fonction pour vérifier et enregistrer le joueur
-	serverID := 1 // Vous pouvez utiliser l'ID de votre serveur ici
+	serverID := 1 // Temporary server ID, will be replaced by the real server ID later
 
-	// Enregistrer la connexion
+	// Save the connection log to the database
 	err := db.SaveConnectionLog(playerName, serverID)
 	if err != nil {
-		fmt.Printf("Erreur lors de l'enregistrement du log de connexion : %v\n", err)
+		return fmt.Errorf("ERROR WHILE SAVING CONNECTION LOG: %v", err)
 	}
+	return nil
 }
